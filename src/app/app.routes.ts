69bf526a627aab92a93e04ccff10729965ee4937
MyNameISBocchi <espinaalfonso123@gmail.com>
2026-05-06 19:47:38 -0400

    guards de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,20 @@ import { Login } from './components/login/login';
 import { ListComunity } from './components/list-comunity/list-comunity';
 import { Home } from './components/home/home';
 import { PersonList } from './components/person-list/person-list';
-import { authGuard } from './guards/auth.guards';
 import { PersonForm } from './components/person-form/person-form';
 
+
+import { authGuard, publicGuard } from './guards/auth.guards'; 
+
 export const routes: Routes = [
-    { path: 'login', component: Login },
+    
+    { path: 'login', component: Login, canActivate: [publicGuard] },
+    
     { path: 'home', component: Home, canActivate: [authGuard] },
     { path: 'person', component: PersonList, canActivate: [authGuard] },
-    { path: 'personForm', component:PersonForm, canActivate:[authGuard]},
+    { path: 'personForm', component: PersonForm, canActivate: [authGuard] },
     { path: 'comunity', component: ListComunity, canActivate: [authGuard] },
     
-    { path: '', redirectTo: '/login', pathMatch: 'full' }
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: '**', redirectTo: '/login' } 
 ];
--- src/app/guards/auth.guards.ts
+++ src/app/guards/auth.guards.ts
@@ -2,6 +2,7 @@ import { inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Auth } from '../services/auth';
 
+
 export const authGuard = () => {
   const authService = inject(Auth);
   const router = inject(Router);
@@ -9,8 +10,19 @@ export const authGuard = () => {
   if (authService.isLoggedIn()) {
     return true;
   } else {
-    
     router.navigate(['/login']);
     return false;
   }
+};
+
+
+export const publicGuard = () => {
+  const authService = inject(Auth);
+  const router = inject(Router);
+
+  if (authService.isLoggedIn()) {
+    router.navigate(['/home']); 
+    return false;
+  }
+  return true;
 };
