ab0021765dadfaf2ea8edde9713d4704ebf58f60
MyNameISBocchi <espinaalfonso123@gmail.com>
2026-05-08 18:39:21 -0400

    createService

--- src/app/components/council-list/council-list.ts
+++ src/app/components/council-list/council-list.ts
@@ -1,11 +1,47 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Council } from '../../services/council';
+
 
 @Component({
   selector: 'app-council-list',
-  imports: [],
+  standalone:true,
+  imports: [CommonModule,FormsModule],
   templateUrl: './council-list.html',
   styleUrl: './council-list.css',
 })
 export class CouncilList {
+  councils:any = [];
+  councilSelect:any = null;
 
+  constructor(private council:Council){}
+
+  ngOnit(){
+    this.getCouncils();
+  }
+
+  getCouncils(){
+    this.council.findAll().subscribe((res:any) => {
+      this.councils = res.results;
+    });
+  }
+
+  createCouncils(event:any){
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = {
+      councilName: formData.get('councilName'),
+      comunityId: formData.get('comunityId'),
+      cityId: formData.get('cityId'),
+      googleMaps:formData.get('googleMaps')
+    };
+
+    this.council.create(data).subscribe((res:any)=>{
+      this.getCouncils();
+      event.target.reset();
+    });
+  }
+
+  
 }
